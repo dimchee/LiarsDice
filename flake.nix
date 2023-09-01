3d0fb498e31134acf27df70e766daa1368be0302
dimchee <dimchee90@gmail.com>
2023-09-01 18:23:34 +0200

    Fix static compilation, no shell

--- flake.nix
+++ flake.nix
@@ -1,30 +1,47 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-	outputs = { self, nixpkgs }:
-	let
-		pkgs = import nixpkgs { system = "x86_64-linux"; };
-		app = pkgs: pkgs.haskellPackages.developPackage {
-			modifier = drv: pkgs.haskell.lib.overrideCabal drv (oa: {
-					buildTools = (oa.buildTools or []) ++ [
-						pkgs.haskellPackages.cabal-install
-						pkgs.haskellPackages.cabal-fmt
-						pkgs.haskellPackages.haskell-language-server
-						pkgs.haskellPackages.fast-tags
-					];
-          enableSharedExecutables = false;
-          enableSharedLibraries = false;
-          configureFlags = [
-            "--ghc-option=-optl=-static"
-            "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
-            "--extra-lib-dirs=${pkgs.zlib.static}/lib"
-            "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
-            "--extra-lib-dirs=${pkgs.ncurses.override { enableStatic = true; }}/lib"
-          ];
-				});
-			root = ./.;
-		};
-	in {
-		defaultPackage.x86_64-linux = app pkgs.pkgsMusl;
-		devShell.x86_64-linux = (app pkgs).env;
-	};
+  description = "A Simple Liars Dice Simulator";
+
+  inputs = {
+    nixpkgs = {
+      url = "https://github.com/nh2/nixpkgs/archive/9e49f8f1f37bc906cda1adb33064c325d760819a.tar.gz";
+      type = "tarball";
+      flake = false;
+    };
+    static-haskell-nix = {
+      url = "github:nh2/static-haskell-nix";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, static-haskell-nix} :
+    let
+      compiler = "ghc927";
+      name = "LiarsDice";
+      system = "x86_64-linux";
+      overlay = self: super: {
+        haskell = super.haskell // {
+          packages = super.haskell.packages // {
+            ${compiler} = super.haskell.packages.${compiler}.override {
+              overrides = final: prev: {${name} = prev.callCabal2nix name ./. { };};
+            };
+          };
+        };
+      };
+      overlays = [overlay];
+      normalPkgs = import nixpkgs {inherit overlays system; };
+      survey = import "${static-haskell-nix}/survey" { inherit compiler normalPkgs; };
+    in
+      {
+        packages.${system}.default = survey.haskellPackages.${name};
+        # devShell.${system} = normalPkgs.mkShell {
+        #   nativeBuildInputs = (survey.haskellPackages.callCabal2nix name ./. {}).nativeBuildInputs;
+        #   buildInputs = 
+        #   (survey.haskellPackages.callCabal2nix name ./. {}).buildInputs ++
+        #   [
+        #     normalPkgs.haskellPackages.cabal-fmt
+        #     normalPkgs.haskellPackages.haskell-language-server
+        #     normalPkgs.haskellPackages.fast-tags
+        #   ];
+        # };
+      };
 }
